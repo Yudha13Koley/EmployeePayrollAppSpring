578630d67bfbf86d43e087367697122e5e027e80
Yudha13Koley <yudhajit1998@gmail.com>
2020-11-27 12:49:18 +0530

    Added : Dto amd model is introduced to the employee payroll app response is given as a form of ResponseDto

---
+++ src/main/java/com/capgemini/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,45 @@
+package com.capgemini.employeepayrollapp.model;
+
+import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
+
+public class EmployeePayrollData {
+
+	private int employeeId;
+	private String name;
+	private double salary;
+
+	public EmployeePayrollData() {
+
+	}
+
+	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDto) {
+		super();
+		this.employeeId = employeeId;
+		this.name = employeePayrollDto.name;
+		this.salary = employeePayrollDto.salary;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
