08d016c9598eba37ecde81079be3d179d4af531f
Yudha13Koley <yudhajit1998@gmail.com>
2020-11-28 11:29:55 +0530

    Added : Other Fields and Validations in model and dto object

--- src/main/java/com/capgemini/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/capgemini/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,12 +1,19 @@
 package com.capgemini.employeepayrollapp.model;
 
+import java.util.Date;
+
 import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
 
 public class EmployeePayrollData {
 
 	private int employeeId;
 	private String name;
+	private String profilePic;
+	private String gender;
+	private String[] department;
 	private double salary;
+	private String note;
+	private Date startDate;
 
 	public EmployeePayrollData() {
 
@@ -17,6 +24,11 @@ public class EmployeePayrollData {
 		this.employeeId = employeeId;
 		this.name = employeePayrollDto.name;
 		this.salary = employeePayrollDto.salary;
+		this.profilePic = employeePayrollDto.profilePic;
+		this.gender = employeePayrollDto.gender;
+		this.department = employeePayrollDto.department;
+		this.note = employeePayrollDto.note;
+		this.startDate = employeePayrollDto.startDate;
 	}
 
 	public int getEmployeeId() {
@@ -35,11 +47,51 @@ public class EmployeePayrollData {
 		this.name = name;
 	}
 
+	public String getProfilePic() {
+		return profilePic;
+	}
+
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String[] getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String[] department) {
+		this.department = department;
+	}
+
 	public double getSalary() {
 		return salary;
 	}
 
 	public void setSalary(double salary) {
 		this.salary = salary;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
 	}
 }
